6870c792d3c32f38a45e2f078295192122ad232f
hydreigon <shunyuu42678@outlook.jp>
2022-11-10 08:38:34 +0900

    11/10

--- Akinter.py
+++ Akinter.py
@@ -1,21 +1,19 @@
-from cgitb import text
+import random
 import tkinter
 import sqlite3
 import cmath
 import time
 
 
-def create_oval(x_1, y_1, x_2, y_2, color: text):
-    canvas.create_oval(x_1 * 5 - 5, y_1 * 5 - 5, x_2 *
+def create_oval(x_1, y_1, x_2, y_2, color: str):
+    return canvas.create_oval(x_1 * 5 - 5, y_1 * 5 - 5, x_2 *
                        5 + 5, y_2 * 5 + 5, fill=color)
 
 
 def set_line():
-    print('aaa')
     cur.execute(
         f"SELECT cross_name_1, cross_name_2, oneway FROM road_info")
     cross_info = cur.fetchall()
-    print(cross_info)
 
     for c in cross_info:
         cur.execute(
@@ -27,16 +25,20 @@ def set_line():
             f"SELECT x, y FROM cross_position WHERE cross_name == '{c[1]}'")
         cross_position_2 = cur.fetchone()
         create_oval(cross_position_1[0], cross_position_1[1],
-                    cross_position_2[0], cross_position_2[1], "#000000")
+                    cross_position_1[0], cross_position_1[1], "#000000")
         if c[2] == 0:
             canvas.create_line(cross_position_1[0] * 5, cross_position_1[1] * 5, cross_position_2[0]
-                               * 5, cross_position_2[1] * 5, tag='line', arrow=tkinter.FIRST)
+                               * 5, cross_position_2[1] * 5, tag='line', arrow=tkinter.BOTH)
         elif c[2] == 1:
             canvas.create_line(cross_position_1[0] * 5, cross_position_1[1] * 5,
                                cross_position_2[0] * 5, cross_position_2[1] * 5, tag='line', arrow=tkinter.LAST)
         elif c[2] == 2:
             canvas.create_line(cross_position_1[0] * 5, cross_position_1[1] * 5, cross_position_2[0]
                                * 5, cross_position_2[1] * 5, tag='line', arrow=tkinter.FIRST)
+
+        canvas.create_text(500, 100, text = "青:now_node", fill= 'blue', width = 200)
+        canvas.create_text(500, 120, text= '赤:現在のノードから移動可能なノード', fill = 'red')
+
 
 
 def euclid(cross_name, goal_name):
@@ -74,13 +76,32 @@ def a_star():
     global disable_nodes
     global now_node
     global node_info
+
     global route_info
     global goal_cross
+    global delete_list
+    
+    time.sleep(1)
+    passed_node = []
+    for d in route_info[now_node]:
+        print("route", d)
+        cur.execute(
+            f"SELECT x, y FROM cross_position WHERE cross_name = '{d}'")
+        a = cur.fetchone()
+        if(passed_node):
+            b = canvas.create_line(passed_node[0] * 5, passed_node[1] * 5, a[0] * 5, a[1] * 5, fill = 'red', width = 3)
+            delete_list.append(b)
+        passed_node = a
 
+    for d in delete_list:
+        canvas.delete(d)
+    delete_list = []
+    cur.execute(
+        f"SELECT x, y FROM cross_position")
+    cross_position = cur.fetchall()
     cur.execute(
         f"SELECT * FROM road_info WHERE cross_name_2 = '{now_node}' AND oneway != 1")
     connect_node_info = [(c[0], c[2]) for c in cur.fetchall()]
-
     cur.execute(
         f"SELECT * FROM road_info WHERE cross_name_1 = '{now_node}' AND oneway != 2")
     connect_node_info += [(c[1], c[2]) for c in cur.fetchall()]
@@ -90,8 +111,17 @@ def a_star():
     have_connect_node = False
     for cn in connect_node_info:
         if cn[0] in disable_nodes:
+            cur.execute(
+                f"SELECT x, y FROM cross_position WHERE cross_name = '{cn[0]}'")
+            res = cur.fetchone()
+            a = create_oval(res[0], res[1], res[0], res[1], 'white')
             continue
 
+        cur.execute(
+            f"SELECT x, y FROM cross_position WHERE cross_name = '{cn[0]}'")
+        res = cur.fetchone()
+        a = create_oval(res[0], res[1], res[0], res[1], 'red')
+        delete_list.append(a)
         # cost = 実際の距離 + ゴールとの直線距離
         dist = node_info[now_node][0] + cn[1]
         cost = euclid(cn[0], goal_cross) + dist
@@ -112,6 +142,11 @@ def a_star():
     disable_nodes.append(now_node)
     node_info.pop(now_node)
     route_info.pop(now_node)
+    cur.execute(
+        f"SELECT x, y FROM cross_position WHERE cross_name = '{now_node}'")
+    cross_position = cur.fetchone()
+    a = create_oval(cross_position[0], cross_position[1], cross_position[0], cross_position[1], 'blue')
+    delete_list.append(a)
 
     # 次の now_node の適任を探す
     min = float('inf')
@@ -120,12 +155,25 @@ def a_star():
         if (node_info[key][1] < min) and (not key in disable_nodes):
             min = node_info[key][1]
             now_node = key
+            
 
     if now_node == goal_cross:
+        cur.execute(
+        f"SELECT x, y FROM cross_position WHERE cross_name = '{now_node}'")
+        cross_position = cur.fetchone()
+        create_oval(cross_position[0], cross_position[1], cross_position[0], cross_position[1], 'yellow')
         # 経路が見つかった場合
         return (True, route_info[goal_cross])
-
+    
     # 経路探索中
+    cur.execute(
+        f"SELECT x, y FROM cross_position WHERE cross_name = '{now_node}'")
+    cross_position = cur.fetchone()
+    global route_point
+    
+    # if route_point:
+    #     canvas.delete(route_point)
+    # route_point = create_oval(cross_position[0], cross_position[1], cross_position[0], cross_position[1], 'yellow')
     return (False, None)
 
 
@@ -136,7 +184,7 @@ def init(start_tag, goal_tag, *disable_nodes: tuple):
     global start_cross
     global goal_cross
 
-    # set_line()
+    set_line()
     start_cross = between(start_tag, True)
     goal_cross = between(goal_tag, False)
 
@@ -150,11 +198,26 @@ def init(start_tag, goal_tag, *disable_nodes: tuple):
         return False
 
 
-def timer_func():
-    res = a_star()
-    print(res)
-    if not res[0]:
-        root.after(3, timer_func)
+def timer_func(flag=False):
+    res = [None]
+    if flag:
+        root.after(100, timer_func)
+    else:
+        res = a_star()
+        print('res[1]' , res)
+        # if(res[1]):
+        #     passed_node = None
+        #     for d in res[1]:
+        #         print("route", d)
+        #         cur.execute(
+        #             f"SELECT x, y FROM cross_position WHERE cross_name = '{d}'")
+        #         a = cur.fetchone()
+        #         if(passed_node):
+        #             canvas.create_line(passed_node[0] * 5, passed_node[1] * 5, a[0] * 5, a[1] * 5, fill = 'red', width = 3)
+        #         passed_node = a
+
+        if not res[0]:
+            root.after(5, timer_func)
 
 
 if __name__ == '__main__':
@@ -170,11 +233,13 @@ if __name__ == '__main__':
 
     # a_star から取り出した
     disable_nodes = []
+    delete_list = []
+    route_point = None
     now_node = None    # 探索中のノード
     node_info = None  # 各ノードの実際の距離とコスト
     route_info = None  # あるノードまでの最短経路
 
     if init("cross_002", "cross_006"):
-        timer_func()
+        timer_func(True)
 
     root.mainloop()
